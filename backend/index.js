417d0e7f773ea7d45320ee3b53148a6c2cdbc375
Gaurav <amvermagaurav007@gmail.com>
2026-02-04 19:54:43 +0530

    add languageConfig to support multiple languages

--- backend/index.js
+++ backend/index.js
@@ -19,17 +19,49 @@ const languageConfig = {
   java: {
     fileName: "Main.java",
     compile: "javac Main.java",
+    run: "java Main",
+  },
+
+  cpp: {
+    fileName: "main.cpp",
+    compile: "g++ main.cpp -o main",
+    run: "./main",
   },
 };
 
 app.post("/run", (req, res) => {
-  const { code } = req.body;
-  fs.writeFileSync("main.py", code); //use sync to block further code exec untill the code is fully written
+  const { language, code } = req.body;
+
+  if (!languageConfig[language]) {
+    return res.status(400).json({
+      error: "Unsupported language",
+    });
+  }
+
+  if (!code) {
+    return res.status(400).json({
+      error: "No code provided",
+    });
+  }
+
+  const config = languageConfig[language];
+
+  //Write code to file
+  fs.writeFileSync(config.fileName, code); //use sync to block further code exec untill the code is fully written
+
+  let command = ""; //Build command
+
+  if (config.compile) {
+    command = `${config.compile} && ${config.run}`;
+  } else {
+    command = config.run;
+  }
+
   //exec - starts a shell
   //runs the command
   //buffers everything printed
   //returns err, stdout, stderr
-  exec("python3 main.py", (err, stdout, stderr) => {
+  exec(command, (err, stdout, stderr) => {
     res.json({
       output: stdout,
       error: stderr,
