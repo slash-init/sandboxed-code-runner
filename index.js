2503e2a398746a1c8e66e32b7e234ebe7d09825a
Gaurav <amvermagaurav007@gmail.com>
2026-02-05 14:53:44 +0530

    refactor : abstract language runtime selection

--- index.js
+++ index.js
@@ -6,73 +6,24 @@ import { spawn } from "child_process";
 const app = express();
 app.use(express.json());
 
-const languageConfig = {
+const runtimes = {
   python: {
-    fileName: "main.py",
-    run: "python3 main.py",
-  },
-
-  js: {
-    fileName: "main.js",
-    run: "node main.py",
-  },
-
-  java: {
-    fileName: "Main.java",
-    compile: "javac Main.java",
-    run: "java Main",
-  },
-
-  cpp: {
-    fileName: "main.cpp",
-    compile: "g++ main.cpp -o main",
-    run: "./main",
+    image: "python-sandbox",
+    file: "main.py",
   },
 };
 
-// app.post("/run", (req, res) => {
-//   const { language, code } = req.body;
-
-//   if (!languageConfig[language]) {
-//     return res.status(400).json({
-//       error: "Unsupported language",
-//     });
-//   }
-
-//   if (!code) {
-//     return res.status(400).json({
-//       error: "No code provided",
-//     });
-//   }
-
-//   const config = languageConfig[language];
-
-//   //Write code to file
-//   fs.writeFileSync(config.fileName, code);
-//   let command = ""; //Build command
-
-//   if (config.compile) {
-//     command = `${config.compile} && ${config.run}`;
-//   } else {
-//     command = config.run;
-//   }
-
-//   exec(command, (err, stdout, stderr) => {
-//     res.json({
-//       output: stdout,
-//       error: stderr,
-//     });
-//   });
-// });
-
 app.post("/run", (req, res) => {
-  const { code } = req.body;
+  const { language, code } = req.body;
+
+  const runtime = runtimes[language];
+  if (!runtime) return res.status(400).json({ error: "Unsupported language" });
 
   const jobId = Date.now().toString();
   const jobDir = path.join("executions", jobId);
 
   fs.mkdirSync(jobDir);
-  fs.writeFileSync(path.join(jobDir, "main.py"), code);
+  fs.writeFileSync(path.join(jobDir, runtime.file), code);
 
   // --rm : runs a container and automatically removes it after execution
   // --cpus=1 : limits the container to use maximum 1 CPU core(prevents CPU exhaustion attacks)
@@ -104,7 +55,7 @@ app.post("/run", (req, res) => {
     "--pids-limit=64",
     "-v",
     `${process.cwd()}/${jobDir}:/app`,
-    "python-sandbox",
+    runtime.image,
   ]);
 
   const TIME_LIMIT = 2000; // ms
